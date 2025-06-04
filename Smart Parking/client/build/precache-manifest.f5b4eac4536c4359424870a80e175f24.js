bbf27622cd23bea735d7a8297c860bfcee09dcab
aadi-dev01 <adityayuvraj9717@gmail.com>
2025-06-04 23:53:09 +0530

    Initial commit: Smart Parking System

---
+++ Smart Parking/client/build/precache-manifest.f5b4eac4536c4359424870a80e175f24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab05be3a47add815367afb15e1b38fa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae457db9c00dc4ccd268",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ae9ff3acda8047e8cd7b",
+    "url": "/static/css/main.7b716316.chunk.css"
+  },
+  {
+    "revision": "ae457db9c00dc4ccd268",
+    "url": "/static/js/2.462df49f.chunk.js"
+  },
+  {
+    "revision": "8423f6c8c0ee5172d16da86f55d023e3",
+    "url": "/static/js/2.462df49f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae9ff3acda8047e8cd7b",
+    "url": "/static/js/main.8c7e922b.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "d344861cc81f561101f4ec402a32f372",
+    "url": "/static/media/cafe.d344861c.jpg"
+  }
+]);
